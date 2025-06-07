3e621f0b1a5a1978cf35814c6784e50767253d4d
Konstantin Tzantchev <tzantchev@ucla.edu>
2025-06-06 23:14:41 -0700

    add basic chat ui

--- src/features/chat/chatview.tsx
+++ src/features/chat/chatview.tsx
@@ -1,8 +1,34 @@
 "use client";
 
-import { VStack, Text, Input, Box } from "@chakra-ui/react";
+import { agent } from "@/lib/agent";
+import { VStack, Text, Input, Box, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function ChatView() {
+  const [isNewChat, setIsNewChat] = useState(true);
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [agentResponse, setAgentResponse] = useState("");
+
+  const onSubmit = async () => {
+    setIsNewChat(false);
+    try {
+      const response = await fetch("/api/chat/write", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ threadId: "default", message: currentMessage }),
+      });
+      if (!response.ok) {
+        throw new Error("Agent error");
+      }
+      const data = await response.json();
+      setAgentResponse(data.answer);
+    } catch (error) {
+      console.error("Error submitting message:", error);
+    }
+  };
+
   return (
     <Box>
       <Text fontSize="xl" fontWeight="semibold" p="4">
@@ -12,7 +38,16 @@ export default function ChatView() {
         <Text fontSize="2xl" fontWeight="semibold">
           Talk to your knowledge base
         </Text>
-        <Input></Input>
+        <Input
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+          }}
+          placeholder="What's in my syllabus for CS 144?"
+        ></Input>
+        <Button onClick={onSubmit} colorScheme="blue">
+          {isNewChat ? "Start Chat" : "Send Message"}
+        </Button>
+        {agentResponse && <Text fontSize="lg">{agentResponse}</Text>}
       </VStack>
     </Box>
   );
--- src/lib/agent.ts
+++ src/lib/agent.ts
@@ -7,7 +7,7 @@ import { MemorySaver } from "@langchain/langgraph";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 
 const agentTools = [new TavilySearch({ tavilyApiKey: process.env.TAVILY_API_KEY, maxResults: 3 })];
-const agentModel = new ChatOpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0 });
+const agentModel = new ChatOpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, model: "gpt-4o-mini", temperature: 0 });
 
 const agentCheckpointer = new MemorySaver();
 export const agent = createReactAgent({
